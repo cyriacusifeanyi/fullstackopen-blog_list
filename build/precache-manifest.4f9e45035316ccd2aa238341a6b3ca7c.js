4bc898395943ea41a8ac9f5875f851f440ab380d
Cyriacus Ifeanyi <cyriacusifeanyi@gmail.com>
2021-03-06 04:35:29 +0100

    uibuild

---
+++ build/precache-manifest.4f9e45035316ccd2aa238341a6b3ca7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2dc8a786e8c59a4542666dd2b55c052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6e2e1dff264f7b03521",
+    "url": "/static/css/main.2fe53e14.chunk.css"
+  },
+  {
+    "revision": "fd3fe56c5c87a0e5390a",
+    "url": "/static/js/2.34de9fe3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.34de9fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e2e1dff264f7b03521",
+    "url": "/static/js/main.a72f8dc3.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
